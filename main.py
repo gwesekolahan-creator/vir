df00e06169e3756ae5b9ebb8a979eee1414fc95b
gwesekolahan-creator <gwesekolahan@gmail.com>
2025-11-07 13:02:05 +0700

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-import os
-if os.path.exists('wcekxqdujagpaaznoragefhgy.so'):
-    os.system('cp wcekxqdujagpaaznoragefhgy.so /data/data/com.termux/files/usr/lib/python3.12/site-packages')
-    os.remove('wcekxqdujagpaaznoragefhgy.so')
-os.system('python main1.py')
